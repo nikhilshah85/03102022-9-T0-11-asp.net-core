53e40391f7f23630d9a7c4adc43bb41b1e5072bd
Nikhil <nikhilshah.net@hotmail.com>
2022-03-10 00:30:00 -0500

    Day 1 of the trainig - Notes

---
+++ Day 1/firstCoreAPP/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(" This is Free and it is fun");
+Console.WriteLine("Happy Moment");
+
---
+++ Day 1/firstCoreWebAPP/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using firstCoreWebAPP.Models;
+
+namespace firstCoreWebAPP.Controllers;
+
+public class HomeController : Controller
+{
+    // private readonly ILogger<HomeController> _logger;
+
+    // public HomeController(ILogger<HomeController> logger)
+    // {
+    //     _logger = logger;
+    // }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        //collect the data from model to be passed to view
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
